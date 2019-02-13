25a2a00f2bef494c9dc2a624968b8c1e3b0f421e
ArturAleksejev <ArturAleksejev@github.com>
2019-02-13 15:05:11 +0200

    Condition switch case

--- php_alused/Condition/index.php
+++ php_alused/Condition/index.php
@@ -1,24 +1,12 @@
 <?php
-$eesnimi = 'Artur';
-$perenimi = "Aleksejev";
-$taisnimi = $eesnimi.' '.$perenimi;
-$vanus = 19;
-$kaal = 65;
-$sugu = 'ei tea';
-
-switch ($sugu) {
-    case 'mees':
-        echo '<p style="color: blue">';
-        break;
-    case 'naine':
-        echo '<p style="color: red">';
-        break;
-    default:
-        echo '<p style="color: green">';
-        break;
+// juhusliku täisarvu genereerime
+$arv = rand(0, 100);
+// arvutame jääk 2-ga jagamisel
+$jaak = $arv % 2;
+echo $arv.' - ';
+if ($jaak == 0) {
+    echo 'paaris';
+}   else {
+    echo 'paaritu';
 }
-//var_dump($eesnimi); // muutuja sisu test vaatamine
-echo 'Mina olen '.$taisnimi.'!<br>';
-echo 'Minu vanus: '.$vanus.'<br>';
-echo 'Minu kaal: '.$kaal.'<br>';
 ?>
