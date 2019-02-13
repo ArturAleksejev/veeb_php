893abdc101d4db1bc80b041f0b73bcda661c93aa
ArturAleksejev <ArturAleksejev@github.com>
2019-02-13 13:08:48 +0200

    Värvilised ringid

--- php_alused/print/index.php
+++ php_alused/print/index.php
@@ -1,17 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
 <?php
-/**
- * Created by PhpStorm.
- * User: artur.aleksejev
- * Date: 13.02.2019
- * Time: 12:15
- */
-echo "Hello World!<br>";
-echo "Hello World!<br>";
-echo "Hello World!<br>";
-
-echo '<hr>';
-
-
-print "Hello World!<br>";
-print "Hello World!<br>";
-print "Hello World!<br>";
+echo '<div id="punane"></div>';
+echo '<div id="kollane"></div>';
+echo '<div id="roheline"></div>';
+?>
+    </body>
+</html>
