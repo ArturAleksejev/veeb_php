94ce88996a274758014def3eec570a3901995425
ArturAleksejev <ArturAleksejev@github.com>
2019-02-13 14:22:46 +0200

    Muutujad näide

---
+++ php_alused/Muutujad/index.php
@@ -0,0 +1,12 @@
+<?php
+$eesnimi = 'Artur';
+$perenimi = "Aleksejev";
+$täisnimi = $eesnimi.' '.$perenimi;
+$vanus = 19;
+$kaal = 65;
+
+//var_dump($eesnimi); // muutuja sisu test vaatamine
+echo 'Tere '.$täisnimi.'!<br>';
+echo 'Minu vanus: '.$vanus.'<br>';
+echo 'Minu kaal: '.$kaal.'<br>';
+?>
