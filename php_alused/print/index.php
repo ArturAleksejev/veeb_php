8140b715397d082e8969883ef2469a57917efbc8
ArturAleksejev <ArturAleksejev@github.com>
2019-02-13 12:39:28 +0200

    Väljastan echo ja käsud

---
+++ php_alused/print/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artur.aleksejev
+ * Date: 13.02.2019
+ * Time: 12:15
+ */
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+
+echo '<hr>';
+
+print "Hello World!<br>";
+print "Hello World!<br>";
+print "Hello World!<br>";
