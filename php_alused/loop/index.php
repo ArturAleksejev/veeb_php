512025e61747f2953b6fe467ec9feb9c07e9d8c1
ArturAleksejev <ArturAleksejev@github.com>
2019-02-19 09:02:40 +0200

    loop erineva värvitega numbrid

--- php_alused/loop/index.php
+++ php_alused/loop/index.php
@@ -1,32 +1,42 @@
 <?php
-    for($kord = 1; $kord <= 10; $kord++) {
-        header(string.'Refresh: 2');
-    }
+header('Refresh: 2');
 ?>
-<style>
-    div {
-        margin: auto;
-        width: 100px;
-        height: 100px;
-        font-size: 70px;
-        padding-top: 15px;
-        text-align: center;
-    }
-    .paaris{
-        background: red;
-    }
-    .paaritu{
-        background: green;
-    }
-</style>
+    <style>
+        div {
+            margin: auto;
+            width: 100px;
+            height: 100px;
+            font-size: 70px;
+            padding-top: 15px;
+            text-align: center;
+        }
+        .paaris{
+            background: red;
+        }
+        .paaritu{
+            background: green;
+        }
+        .algarv{
+            background: deepskyblue;
+        }
+        .tavaline{
+            background: lightgrey;
+        }
+    </style>
 <?php
 // genereerime juhuslik täisarv vahemikus 0 kuni 100
 $arv = rand(0, 100);
-// arvutame jääk 2-ga jagamisel
-$jaak = $arv % 2;
-// kontrollime kui jääk on 0 - paaris arv
-if($jaak == 0){
-    echo '<div class="paaris">'.$arv.'</div>';
+// defineerime jagaja algväärtus
+$jagaja = 2;
+// nii kaua kui jagamise jääk ei ole null
+while($arv % $jagaja != 0){
+    // võtame järgmine jagaja väärtus
+    $jagaja++; // $jagaja = $jagaja + 1;
+}
+// kui $arv ja $jagaja on võrdsed - see on algarv
+// muidu mitte
+if ($arv == $jagaja) {
+    echo '<div class="algarv">' . $arv . '</div>';
 } else {
-    echo '<div class="paaritu">'.$arv.'</div>';
+    echo '<div class="tavaline">' . $arv . '</div>';
 }
