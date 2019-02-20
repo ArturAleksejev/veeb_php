d52034d53507e4dc750fb1e13d04549a862270e5
ArturAleksejev <ArturAleksejev@github.com>
2019-02-20 13:48:04 +0200

    loop

--- php_alused/loop/index.php
+++ php_alused/loop/index.php
@@ -1,47 +1,71 @@
-
-    <style>
-        div {
-            margin: auto;
-            width: 100px;
-            height: 100px;
-            font-size: 70px;
-            padding-top: 15px;
-            text-align: center;
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Arvud</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossorigin="anonymous">
+</head>
+<body>
+<div class="container text-center">
+    <!-- tabel -->
+    <table class="table">
+        <!-- tabeli header -->
+        <thead>
+        <tr>
+            <th>arv</th>
+            <th>paaris</th>
+            <th>paaritu</th>
+            <th>algarv</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        for($arv = 0; $arv <= 15; $arv++){
+            if($arv % 2 == 0) {
+                $kasPaaris = true;
+            } else {
+                $kasPaaris = false;
+            }
+            $jagaja = 2;
+            if($arv == 0 or $arv == 1) {
+                $kasAlgarv = true;
+            } else {
+                while ($arv % $jagaja != 0) $jagaja++;
+                if ($arv == $jagaja) {
+                    $kasAlgarv = true;
+                } else {
+                    $kasAlgarv = false;
+                }
+            }
+            echo '
+                    <tr>
+                        <td>'.$arv.'</td>
+                        <td class="paaris">';
+            if($kasPaaris) {
+                echo '<i class="fas fa-angle-down"></i>';
+            }
+            echo '</td>';
+            echo '<td class="paaritu">';
+            if(!$kasPaaris) {
+                echo '<i class="fas fa-angle-down"></i>';
+            }
+            echo '</td>';
+            echo '<td class="algarv">';
+            if($kasAlgarv) {
+                echo '<i class="fas fa-angle-down"></i>';
+            }
+            echo '</td>';
+            echo '</tr>';
         }
-        .paaris{
-            background: red;
-        }
-        .paaritu{
-            background: green;
-        }
-        .algarv{
-            background: deepskyblue;
-        }
-        .tavaline{
-            background: lightgrey;
-        }
-    </style>
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
 <?php
-// genereerime juhuslik täisarv vahemikus 0 kuni 100
-$arv = rand(0, 100);
-// defineerime jagaja algväärtus
-$jagaja = 2;
-// nii kaua kui jagamise jääk ei ole null
-while($arv % $jagaja != 0){
-    // võtame järgmine jagaja väärtus
-    $jagaja++; // $jagaja = $jagaja + 1;
-}
-// kui $arv ja $jagaja on võrdsed - see on algarv
-// muidu mitte
-if ($arv == $jagaja) {
-    echo '<div class="algarv">' . $arv . '</div>';
-} else {
-    echo '<div class="tavaline">' . $arv . '</div>';
-}
-$jaak = $arv % 2;
-// kontrollime kui jääk on 0 - paaris arv
-if($jaak == 0){
-    echo '<div class="paaris">'.$arv.'</div>';
-} else {
-    echo '<div class="paaritu">'.$arv.'</div>';
-}
