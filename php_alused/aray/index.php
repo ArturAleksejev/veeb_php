267ac2cc39d7d4229b2bcac0a71c8bb896a69eb5
ArturAleksejev <ArturAleksejev@github.com>
2019-02-25 10:24:51 +0200

    raamatud array tabelis

--- php_alused/aray/index.php
+++ php_alused/aray/index.php
@@ -1,48 +1,33 @@
 <?php
-$peppa = array(
-    'nimi' => 'Peppa',
-    'sugu' => 'naine',
-    'vanus' => 4,
-    'pikkus' => 1.04
-);
-$george = array(
-    'nimi' => 'George',
-    'sugu' => 'mees',
-    'vanus' => 2,
-    'pikkus' => 0.95
-);
-
-foreach ($peppa as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-
-foreach ($george as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-
-echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
-echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
-echo '<hr>';
-
-$porsad = array();
-$porsad['peppa'] = $peppa;
-$porsad['george'] = $george;
-
-$porsad['peppa']['lemmik varv'] = 'punane';
-$porsad['george']['lemmik varv'] = 'sinine';
-
-foreach ($porsad as $porsaseNimi=>$porsaseAndmed){
-    if($porsaseAndmed['sugu'] == 'naine'){
-        echo '<p style="color: red">';
-    } else {
-        echo '<p style="color: blue">';
+function build_table($array){
+    // start table
+    $html = '<table>';
+    // header row
+    $html .= '<tr>';
+    foreach($array[0] as $key=>$value){
+        $html .= '<th>' . htmlspecialchars($key) . '</th>';
     }
-    echo '<b>'.$porsaseNimi.'</b></p>';
-    echo '<ul>';
-    foreach ($porsaseAndmed as $nimetus=>$vaartus){
-        echo '<li>'.$nimetus.' - '.$vaartus.'</li>';
+    $html .= '</tr>';
+
+    // data rows
+    foreach( $array as $key=>$value){
+        $html .= '<tr>';
+        foreach($value as $key2=>$value2){
+            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+        }
+        $html .= '</tr>';
     }
-    echo '</ul>';
+
+    // finish table and return it
+
+    $html .= '</table>';
+    return $html;
 }
+
+$raamatud = array(
+    array('Nimi'=>'Enesehinnangu tööraamat teismelistele', 'Autor'=>'Lisa M. Schab', 'Keel'=>'eesti', 'Lehekulgi'=>192),
+    array('Nimi'=>'Põrsas Peppa. Esimesed numbrid', 'Autor'=>'eOne', 'Keel'=>'eesti', 'Lehekulgi'=>10),
+    array('Nimi'=>'Jänku-Jass mängib peitust', 'Autor'=>'Julia Sigarova', 'Keel'=>'eesti', 'Lehekulgi'=>12)
+);
+echo build_table($raamatud);
+?>
