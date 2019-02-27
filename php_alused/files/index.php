d0644c5d40d18001c79eb8c38a34644aa3865c62
ArturAleksejev <ArturAleksejev@github.com>
2019-02-27 14:40:09 +0200

    teksti failist lugemine

--- php_alused/files/index.php
+++ php_alused/files/index.php
@@ -8,10 +8,31 @@ function failiKontroll($failiNimi){
 function loeFailist($failiNimi){
     if(failiKontroll($failiNimi)){
         $fp = fopen($failiNimi, 'r');
-        while($fp){
+        while(!feof($fp)){
             $rida = fgets($fp);
-            echo $rida.'<br>';
+            if(trim($rida) == ''){
+                echo '=======================<br>';
+            } else {
+                echo $rida . '<br>';
+            }
         }
     }
 }
+
+function tabelFailist($failiNimi) {
+    if(failiKontroll($failiNimi)){
+        $fp = fopen($failiNimi, 'r');
+        echo '<table border="5">';
+            echo '<tr>';
+                while(!feof($fp)) {
+                    $rida = fgets($fp);
+                    if(trim($rida) != ''){
+                        echo '<td>'.$rida.'<td>';
+                    } else {
+                        echo '</tr>';
+                    }
+                }
+    }           echo '</table>';
+}
 loeFailist('raamatud.txt');
+tabelFailist('raamatud.txt');
